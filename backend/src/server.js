4848602bcceeedd0e37100cac13a1509adcbfb0f
rafael1199v <123646379+rafael1199v@users.noreply.github.com>
2025-06-16 20:05:43 -0400

    Cambios con backend

---
+++ backend/src/server.js
@@ -0,0 +1,28 @@
+import { config } from 'dotenv';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import morgan from "morgan";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+config({ path: path.join(__dirname, '../.env')});
+const app = express();
+
+const port = 3000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../../frontend')));
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.sendFile(path.join(__dirname, '../../frontend/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+});
