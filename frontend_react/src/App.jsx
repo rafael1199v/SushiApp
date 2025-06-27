f660c1b89f2c55cc922d540a8c245a6b60ec2c79
rafael1199v <123646379+rafael1199v@users.noreply.github.com>
2025-06-26 23:27:17 -0400

    Configuracion de assets y obtención de estilos del layout

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,9 +1,11 @@
+import { RouterProvider } from "react-router-dom"
+import router from "./router"
 
 function App() {
   
   return (
     <>
-      <h1>Hola</h1>
+      <RouterProvider router={router}/>
     </>
   )
 }
---
+++ frontend_react/src/pages/LayoutPage.jsx
@@ -0,0 +1,56 @@
+import { Outlet } from "react-router-dom";
+
+function LayoutPage() {
+  return (
+    <section className="layout-page">
+        <div className="layout-page__image-wrapper">
+            <navbar-menu className="layout-page__menu"></navbar-menu>
+            <p className="layout-page__title">SUSHI <br/> SENSATION</p>
+
+            <div className="layout-page__socials">
+                <badge-icon src="/assets/img/instagram-logo-light.svg" width="18" height="18"></badge-icon>
+                <badge-icon src="/assets/img/facebook-logo-light.svg" width="18" height="18"></badge-icon>
+                <badge-icon src="/assets/img/twitter-logo-light.svg" width="18" height="18"></badge-icon>
+            </div>
+
+            <div className="layout-page__account">
+                <div className="layout-page__account-icon">
+                    <img src="/assets/img/iconamoon_profile-fill.svg" className="layout-page__icon-person"/>
+                </div>
+                <button className="layout-page__account-register">
+                    REGISTRATION    
+                </button>
+
+                <div className="layout-page__account-icon layout-page__account-icon--cart">
+                    <img src="/assets/img/cart.svg" className="layout-page__icon-cart"/>
+                    <div className="layout-page__icon-cart-count">0</div>
+                </div>
+            </div>
+
+
+            <div className="layout-page__add-button">
+                <svg width="40" height="40" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11.8334 6.83171H6.83335V11.8317H5.16669V6.83171H0.166687V5.16504H5.16669V0.165039H6.83335V5.16504H11.8334V6.83171Z" fill="#272723" className="layout-page__add-icon-fill"/>
+                </svg>
+            </div>
+        </div>
+        
+        <div className="layout-page__content">
+            <div className="layout-page__content-main">
+              <Outlet />
+            </div>
+
+            <footer className="layout-page__footer">
+                <div className="layout-page__footer-copyright">
+                    <p>Licensing</p>
+                    <img src="/assets/img/diamondIcon.svg" className="modal-menu__logo-diamond"/>
+                    <p>Styleguide</p>
+                </div>
+            </footer>
+        </div>
+
+    </section>
+  )
+}
+
+export default LayoutPage
